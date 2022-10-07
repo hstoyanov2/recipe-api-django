35bef3092cc690fdef5757e0d0a290586db066d2
Hristo Stoyanov <hstoyanov@Hristos-MacBook-Pro.local>
2022-10-07 15:53:30 +0300

    Configured database and test for database connection

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -10,7 +10,7 @@ class ModelTests(TestCase):
 
     def test_create_user_with_email_seccessful(self):
         """Test creating a user with an email is successful."""
-        email='test@example.com'
+        email = 'test@example.com'
         password = 'testpass123'
         user = get_user_model().objects.create_user(
             email=email,
@@ -22,14 +22,14 @@ class ModelTests(TestCase):
 
     def test_new_user_email_normalized(self):
         """Test email is normalized for new users."""
-        sample_emails= [
+        sample_emails = [
             ['test1@EXAMPLE.com', 'test1@example.com'],
             ['test2@example.com', 'test2@example.com']
         ]
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
-    
+
     def test_new_user_without_email_raises_error(self):
         """Test that creating a user without an email raises a ValueError."""
         with self.assertRaises(ValueError):
